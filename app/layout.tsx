f7360965e81bb4aaac3e2d56de47879dea6109a6
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2025-10-24 14:03:16 -0700

    first commut

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Calendra",
+  description: "Calendra is a simple and efficient calendar app",
+};
+
+//login route
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased animate-fade-in`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <h1 className="font-bold text-center">Calendar app</h1>;
+}

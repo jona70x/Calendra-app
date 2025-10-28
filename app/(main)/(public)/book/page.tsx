c3e30415a9282e7b2d620009e52efdd053010ceb
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2025-10-28 16:41:14 -0700

    created loading component to show a state when data is loading. Create profile page where users will show their availability

--- app/(main)/(public)/book/page.tsx
+++ app/(main)/(public)/book/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <h1>Book Page</h1>;
+import Loading from "@/components/Loading";
+import { useUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default function PublicPage() {
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    // show loading until data is available
+    return <Loading />;
+  }
+
+  if (!user) {
+    return redirect("/login");
+  }
+  // public page
+  return redirect(`/book/${user.id}`);
 }
